460ef28b4339d27ff3fdc18941279873f3accb24
jessy <j.alene.wilson@gmail.com>
2023-01-06 14:58:34 -0500

    Update pwd-demo.py

--- pwd-demo.py
+++ pwd-demo.py
@@ -6,8 +6,4 @@ print('Current Working Directory is: ', cwd)
 absolute_path = 'Queries'
 os.chdir(absolute_path)
 
-print('New working directory is: ', os.getcwd())
-
-# Returns:
-# Current Working Directory is:  /Users/datagy
-# New working directory is:  /Users/datagy/Documents
+print('New working directory is: ', os.getcwd()) 
