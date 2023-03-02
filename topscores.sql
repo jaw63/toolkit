d3ecf39a241cdf9a194c672f3a94ee86ce14475c
jessy <j.alene.wilson@gmail.com>
2023-03-02 10:39:11 -0500

    Delete topscores.sql

--- topscores.sql
+++
@@ -1,9 +0,0 @@
-WITH TopScores AS (
-  SELECT student_name, test_score, date, subject,
-         ROW_NUMBER() OVER (PARTITION BY student_name ORDER BY test_score DESC) AS row_num
-  FROM Scores
-)
-SELECT student_name, test_score, date, subject
-FROM TopScores
-WHERE row_num <= 3
-ORDER BY student_name, test_score DESC;
