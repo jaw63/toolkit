3235ee0cbc44ff79ad083e05f6e6691085b90ccc
jessy <j.alene.wilson@gmail.com>
2023-03-17 12:40:06 -0400

    Update csv_col_count.py

--- Python Toolkit/csv_col_count.py
+++ Python Toolkit/csv_col_count.py
@@ -1,17 +1,29 @@
 import csv
 
-with open('filename.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    
-    # Count the number of rows
-    row_count = sum(1 for row in csvreader)
-    
-    # Reset the file pointer to the beginning of the file
+filename = 'file.csv'
+
+# open CSV file
+with open(filename, newline='') as csvfile:
+    # create CSV reader object
+    reader = csv.reader(csvfile)
+
+    # count number of rows and columns
+    num_rows = sum(1 for row in reader)
     csvfile.seek(0)
-    
-    # Count the number of columns in the first row
-    col_count = len(next(csvreader))
-    
-    # Print the row and column counts
-    print(f"Number of rows: {row_count}")
-    print(f"Number of columns: {col_count}")
+    num_cols = len(next(reader))
+
+    # print number of rows and columns
+    print("Number of rows: ", num_rows)
+    print("Number of columns: ", num_cols)
+
+    # print header row
+    csvfile.seek(0)
+    header = next(reader)
+    print("Header: ", header)
+
+    # print first few lines
+    for i, row in enumerate(reader):
+        if i < 5: # adjust the number of rows to print as needed
+            print(row)
+        else:
+            break
