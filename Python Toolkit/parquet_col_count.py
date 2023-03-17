25500110005a2e5e3c028b1af58ec31d2542c315
jessy <j.alene.wilson@gmail.com>
2023-03-17 12:23:25 -0400

    Update parquet_col_count.py

--- Python Toolkit/parquet_col_count.py
+++ Python Toolkit/parquet_col_count.py
@@ -14,3 +14,9 @@ num_cols = len(parquet_file.schema)
 # Print the dimensions
 print(f"Number of rows: {num_rows}")
 print(f"Number of columns: {num_cols}")
+
+# Print the first few lines
+table = pq.read_table(filename)
+first_four_rows = table.slice(0, 4)
+df = first_four_rows.to_pandas()
+print(df)
