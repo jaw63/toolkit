ad15a7541187d20c899e21ce58ad6dc2be7b17d1
jessy <j.alene.wilson@gmail.com>
2023-05-02 13:59:04 -0400

    Create parquet_convert_to_CSV.py

---
+++ Parquet/parquet_convert_to_CSV.py
@@ -0,0 +1,17 @@
+import pyarrow.parquet as pq
+import pandas as pd
+
+# Specify the path to the Parquet file
+parquet_file = 'c360_counts.parquet'
+
+# Read the Parquet file into a PyArrow Table
+table = pq.read_table(parquet_file)
+
+# Convert the PyArrow Table to a Pandas DataFrame
+df = table.to_pandas()
+
+# Specify the path and filename for the CSV file
+csv_file = 'c360_counts.csv'
+
+# Write the Pandas DataFrame to a CSV file
+df.to_csv(csv_file, index=False)
